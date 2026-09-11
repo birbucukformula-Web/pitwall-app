2acc4022eb3db5cecde0626c018b7724d2220aa1
kucukrumeysa <33rumeysakucuk@gmail.com>
2026-09-11 21:56:12 +0300

    feat: add username field to admin panel user creation form

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -1,17 +1,36 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm
 from .models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('email',)
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        # username alanı zorunlu olduğu için email'den otomatik türet
+        if not user.username:
+            user.username = user.email.split('@')[0]
+        if commit:
+            user.save()
+        return user
 
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+
     list_display = ('email', 'first_name', 'last_name', 'role', 'organization', 'is_staff')
     list_filter = ('role', 'organization', 'is_staff', 'is_active')
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
 
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {'fields': ('email', 'username', 'password')}),
         ('Kişisel Bilgiler', {'fields': ('first_name', 'last_name')}),
         ('PitWall Özel Alanlar', {'fields': ('role', 'organization')}),
         ('İzinler & Durum', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
@@ -21,6 +40,6 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password', 'role', 'organization', 'is_staff', 'is_active'),
+            'fields': ('email', 'password1', 'password2', 'role', 'organization', 'is_staff', 'is_active'),
         }),
     )
